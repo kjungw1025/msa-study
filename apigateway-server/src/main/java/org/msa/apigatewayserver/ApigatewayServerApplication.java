4135ac25b315b155eb2972c2983dc02b364706c0
kjungw1025 <kjungw1025@naver.com>
2024-04-16 14:14:55 +0900

    conf: Setting API Gateway Server

---
+++ apigateway-server/src/main/java/org/msa/apigatewayserver/ApigatewayServerApplication.java
@@ -0,0 +1,13 @@
+package org.msa.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServerApplication.class, args);
+	}
+
+}
